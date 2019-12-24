da21b626356251fd7690e1daa59e7840e61c0d2f
ichater <chaterizaak@gmail.com>
2019-12-25 10:17:03 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import PersonalDetails from "./Components/PersonalDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {" "}
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+          <Tab>Title 3</Tab>
+        </TabList>
+        <TabPanel>
+          <PersonalDetails />
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+        <TabPanel>
+          <PersonalDetails />
+        </TabPanel>
+      </Tabs>
+    </>
   );
 }
 
---
+++ src/Components/PersonalDetails.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PersonalDetails = () => {
+  return <>Hello world</>;
+};
+
+export default PersonalDetails;
